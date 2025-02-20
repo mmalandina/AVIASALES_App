7d593f59f79c330a65c740172b33f7c024a5c772
mmalandina <105917684+mmalandina@users.noreply.github.com>
2025-02-20 10:34:37 +0300

    img

--- src/Components/App/app.jsx
+++ src/Components/App/app.jsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from '../../Store/store';
-import Checkboxes from '../Checkboxes';
-import TicketsFilter from '../TicketsFilter';
-import TicketsList from '../TicketsList';
-import ShowMore from '../ShowMore';
-import './app.scss';
+import { Provider } from "react-redux";
+import store from "../../Store/store";
+import Checkboxes from "../Checkboxes";
+import TicketsFilter from "../TicketsFilter";
+import TicketsList from "../TicketsList";
+import ShowMore from "../ShowMore";
+import "./app.scss";
 
 export default function App() {
   return (
     <Provider store={store}>
       <div className="app">
         <header className="header">
-          <img
-            src="src\Components\img\Logo.svg"
-            alt="Logo"
-            className="logo"
-          />
+          <img src="src/Components/img/Logo.svg" alt="Logo" className="logo" />
         </header>
         <main className="main">
           <div className="checkboxes-wrap">
